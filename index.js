78de60b2ae8d9284b9d4d5afb01d2f6a70679124
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2018-11-27 11:24:48 +0100

    testy hesty

---
+++ index.js
@@ -0,0 +1,14 @@
+//importing the node framework 
+var express = require('express');
+
+var app = express();
+
+//respond with "hello world" for requests that go to our root "/"
+app.get('/', function(req, res) {
+    res.send('hello world');
+});
+
+//listen to port 3000 as a default port, this can by changed later in the code this is just as a default
+app.listen (process.env.PORT || 3000);
+
+module.exports = app;
